aeb2e3452a967730c3bed5751e09c57d28f91fcb
Kleber Bastos <bastoskp@hotmail.com>
2021-04-26 20:03:06 -0300

    removeu modelo de orderPlaced e atualizou relação user orderplaced

--- models/OrdersPlaced.model.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-const OrdersPlacedSchema = new Schema({
-    total: Number,
-    cart: { type: Schema.Types.ObjectId, ref: "cart" },
-}, {
-    timestamps: true,
-});
-
-module.exports = model("ordersPlaced", OrdersPlacedSchema);
