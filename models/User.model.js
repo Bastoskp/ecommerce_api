5b5abdb77ff313fb0fa3ee8cf00f9e00ef5b31ed
Fernanda Delleprane <fernandadelleprane@gmail.com>
2021-04-29 12:43:25 -0300

    Update Backend

--- models/User.model.js
+++ models/User.model.js
@@ -1,25 +1,25 @@
 const { Schema, model } = require("mongoose");
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    require: [true, "nome obrigatório"],
-  },
+    name: {
+        type: String,
+        require: [true, "nome obrigatório"],
+    },
 
-  email: {
-    type: String,
-    require: [true, "email obrigatório"],
-  },
+    email: {
+        type: String,
+        require: [true, "email obrigatório"],
+    },
 
-  passwordHash: {
-    type: String,
-    required: [true, "favor colocar sua senha"],
-  },
-  firstName: String,
-  lastName: String,
-  telefone: Number,
-  requests: [{ type: Schema.Types.ObjectId, ref: "requests" }],
-  favorites: [{ type: Schema.Types.ObjectId, ref: "product" }],
-  cart: [{ type: Schema.Types.ObjectId, ref: "cart" }],
+    passwordHash: {
+        type: String,
+        required: [true, "favor colocar sua senha"],
+    },
+    firstName: String,
+    lastName: String,
+    telefone: Number,
+    requests: [{ type: Schema.Types.ObjectId, ref: "requests" }],
+    favorites: [{ type: Schema.Types.ObjectId, ref: "product" }],
+    cart: [{ type: Schema.Types.ObjectId, ref: "cart" }],
 });
 
 module.exports = model("user", UserSchema);
--- routes/perfil.route.js
+++ routes/perfil.route.js
@@ -5,81 +5,81 @@ const Address = require("../models/Address.model");
 const router = require("./homeRoutes.js");
 const perfilRouter = Router();
 
-perfilRouter.get("/perfil/:id", async (req, res) => {
-  try {
-    const user = req.body;
-    const { id } = req.params;
-    const perfil = await User.findById(id);
-    res.status(200).json(perfil);
-  } catch (error) {
-    res.status(500).json({ message: "Erro ao buscar informações do usuàrio" });
-  }
+perfilRouter.get("/perfil/:id", async(req, res) => {
+    try {
+        const user = req.body;
+        const { id } = req.params;
+        const perfil = await User.findById(id);
+        res.status(200).json(perfil);
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao buscar informações do usuàrio" });
+    }
 });
 
 //CRIAR UM NOVO ENDERECO
 
-perfilRouter.post("/perfil/:id/createaddress", async (req, res) => {
-  const { id } = req.params;
-  const newAddress = { ...req.body, userId: id };
-  try {
-    const createdAddress = await Address.create(newAddress);
-    res.status(201).json(createdAddress);
-  } catch (error) {
-    res.status(500).json({ message: "Error while create new Adress" });
-  }
+perfilRouter.post("/perfil/:id/createaddress", async(req, res) => {
+    const { id } = req.params;
+    const newAddress = {...req.body, userId: id };
+    try {
+        const createdAddress = await Address.create(newAddress);
+        res.status(201).json(createdAddress);
+    } catch (error) {
+        res.status(500).json({ message: "Error while create new Adress" });
+    }
 });
 
 // Mostrar todos os endereços do usuario
-perfilRouter.get("/perfil", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const listAddress = await Address.find(id);
-    res.status(200).json(listAddress);
-  } catch (error) {
-    res.status(500).json({ message: "Server side error on get address" });
-  }
+perfilRouter.get("/perfil/address/:id", async(req, res) => {
+    const { id } = req.params;
+    try {
+        const listAddress = await Address.find({ userId: id });
+        res.status(200).json(listAddress);
+    } catch (error) {
+        res.status(500).json({ message: "Server side error on get address" });
+    }
 });
 
 //EDITAR UM ENDERECO
 
-perfilRouter.patch("/perfil/:id", async (req, res) => {
-  try {
-    const address = req.body;
-    const { id } = req.params;
-    const patchAddress = await Address.findByIdAndUpdate(id, address, {
-      new: true,
-    });
-    res.status(201).json(patchAddress);
-  } catch (error) {
-    res.status(500).json({ message: "Server side error on update Address" });
-  }
+perfilRouter.patch("/perfil/:id", async(req, res) => {
+    try {
+        const address = req.body;
+        const { id } = req.params;
+        const patchAddress = await Address.findByIdAndUpdate(id, address, {
+            new: true,
+        });
+        res.status(201).json(patchAddress);
+    } catch (error) {
+        res.status(500).json({ message: "Server side error on update Address" });
+    }
 });
 
 //DELETAR UM ENDERECO
 
-perfilRouter.delete("/perfil/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    await Address.findByIdAndRemove(id);
-    res.status(204).json();
-  } catch (error) {
-    res.status(500).json({ message: "Server side error on delete adress" });
-  }
+perfilRouter.delete("/perfil/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        await Address.findByIdAndRemove(id);
+        res.status(204).json();
+    } catch (error) {
+        res.status(500).json({ message: "Server side error on delete adress" });
+    }
 });
 
-perfilRouter.get("/perfil/minhascompras/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const userCart = await User.findById(id).populate("cart");
-    console.log("carrinho do usuario", userCart);
-    const finishedCarts = userCart.cart.filter(
-      (cart) => cart.finished === true
-    );
-    res.status(200).json(finishedCarts);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "erro ao trazer minhas compras" });
-  }
+perfilRouter.get("/perfil/minhascompras/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const userCart = await User.findById(id).populate("cart");
+        console.log("carrinho do usuario", userCart);
+        const finishedCarts = userCart.cart.filter(
+            (cart) => cart.finished === true
+        );
+        res.status(200).json(finishedCarts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "erro ao trazer minhas compras" });
+    }
 });
 
 module.exports = perfilRouter;
